9009f85f17c3d92416de07d99b7526bef0fc68a1
Maria <mariarosekesa@gmail.com>
2023-07-19 17:36:57 +0300

    image

--- components/BrainwideNeuropixels.svelte
+++ components/BrainwideNeuropixels.svelte
@@ -7,39 +7,69 @@
 	onMount(async () => {
 		const d3 = await import('d3');
 
-		// Load the JSON data;
-		//const response = await fetch('/public/neuropixel_probe_locs.json');
-
-		//const data = await response.json();
-
-		// Set up the SVG container
-		const width = 800;
-		const height = 800;
+		// Set up the SVG container for the scatter plot
+		const width = 500;
+		const height = 500;
 
 		const svg = d3.select(chart).append('svg').attr('width', width).attr('height', height);
 
 		// Create scales for x and y coordinates
 		const xScale = d3
 			.scaleLinear()
-			.domain([0, d3.max(data, (d) => d.x)])
+			.domain([-d3.max(data, (d) => d.x), d3.max(data, (d) => d.x)])
 			.range([0, width]);
 
 		const yScale = d3
 			.scaleLinear()
-			.domain([0, d3.max(data, (d) => d.y)])
+			.domain([-d3.max(data, (d) => d.y), d3.max(data, (d) => d.y)])
 			.range([height, 0]);
 
 		// Create the scatter plot
-		svg
+		const circles = svg
 			.selectAll('circle')
 			.data(data)
 			.enter()
 			.append('circle')
 			.attr('cx', (d) => xScale(d.x))
 			.attr('cy', (d) => yScale(d.y))
-			.attr('r', 5)
-			.attr('fill', 'steelblue');
+			.attr('r', 3.5)
+			.attr('fill', '#34ebb7');
+
+		// Event listener for hover over circles
+		circles.on('mouseover', async (event, d) => {
+			const imageSrc = `/brainwide-neuropixels-vis/${encodeURIComponent(d.image_filename)}`; // Construct the image URL
+			const imageBlob = await fetch(imageSrc).then((response) => response.blob()); // Fetch the image as a Blob
+			const imageUrl = URL.createObjectURL(imageBlob); // Create a temporary URL for the image Blob
+			updateImage(imageUrl);
+		});
+
+		// Set up the SVG container for the image
+		const imageWidth = 300; // Set the desired width for the image
+		const imageHeight = 200; // Set the desired height for the image
+
+		const imageSvg = d3
+			.select(chart)
+			.append('svg')
+			.attr('width', imageWidth)
+			.attr('height', imageHeight);
+
+		// Append the image element
+		const imageElement = imageSvg
+			.append('image')
+			.attr('x', 0)
+			.attr('y', 0)
+			.attr('width', imageWidth)
+			.attr('height', imageHeight);
+
+		// Function to update the image
+		const updateImage = (src) => {
+			imageElement.attr('xlink:href', src);
+		};
 	});
 </script>
 
 <div bind:this={chart} />
+
+<style>
+	/* Your custom styles for the scatter plot and image container can go here */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script>
 	import BrainwideNeuropixels from '../../components/BrainwideNeuropixels.svelte';
-
-	//const scatterData = [
-	//{ x: 10, y: 20 },
-	//{ x: 30, y: 40 },
-	//{ x: 50, y: 60 }
-	// Add more data points as needed
-	//];
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>My blog!</p>
+<h1>SystemsAndNeurons</h1>
 
+<h2 id="brain-wide-neuropixels">ONE Neuropixel probe API probe insertions</h2>
+
+<p>Algorithm: tSNE embedding</p>
+<p>Python code:</p>
+<p>Svelte component:</p>
 <BrainwideNeuropixels />
